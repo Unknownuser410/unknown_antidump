e46d57bad9626ea3f5159605493ddae0f30e6bea
Unknown_user410 <christianhenrich16@gmail.com>
2025-02-09 01:58:08 +0100

    Änderungen an example.lua

--- client/example.lua
+++ client/example.lua
@@ -1,7 +1,12 @@
 local resource = GetCurrentResourceName()
 
 RegisterNetEvent(resource.."unknown_antidump:client:load", function(code)
-    local cd,err = assert(load(code,"Scripterror erkannt: "))() 
+    local func, err = load(code)
+    if func then
+        func() -- Führt den geladenen Code aus
+    else
+        print("Fehler beim Laden des Codes:", err)
+    end
 end)
 
 Wait(100)
