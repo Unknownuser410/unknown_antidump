e2e74075af1d42511f733753b0f268036b90bff4
Unknown_user410 <christianhenrich16@gmail.com>
2025-02-09 03:01:59 +0100

    Sicherheitsupdate

--- client/example.lua
+++ client/example.lua
@@ -1,12 +1,14 @@
 local securitykey = math.random(111111,999999)
 local resource = GetCurrentResourceName()
+local codeloaded = false
 
 RegisterNetEvent(resource.."unknown_antidump:client:load", function(code,key)
-    if key ~= securitykey then
+    if key ~= securitykey or codeloaded then
         print("Netter Versuch!")
         return
     end
     assert(load(code, "Error:"))() --Load and execute Code
+    codeloaded = true
 end)
 
 Wait(100)
