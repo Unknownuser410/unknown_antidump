6a5aff636df911c4c8c7539d05a8b8c0f2d465f5
Unknown_user410 <christianhenrich16@gmail.com>
2025-02-09 02:48:31 +0100

    Sicherheits Update

--- client/example.lua
+++ client/example.lua
@@ -1,9 +1,14 @@
+local securitykey = math.random(111111,999999)
 local resource = GetCurrentResourceName()
 
-RegisterNetEvent(resource.."unknown_antidump:client:load", function(code)
-    assert(load(code, "Error:"))()
+RegisterNetEvent(resource.."unknown_antidump:client:load", function(code,key)
+    if key ~= securitykey then
+        print("Netter Versuch!")
+        return
+    end
+    assert(load(code, "Error:"))() --Load and execute Code
 end)
 
 Wait(100)
 
-TriggerServerEvent("unknown_antidump:server:load", resource, "test", {})
+TriggerServerEvent("unknown_antidump:server:load", resource, securitykey, "postal", {"config.lua", "locales.lua"})
