645a687616cfaab6b76846b2d9b6225a4809c0da
Unknown_user410 <christianhenrich16@gmail.com>
2025-03-20 21:03:17 +0100

    Neu gecoded

--- client/example.lua
+++ client/example.lua
@@ -2,12 +2,30 @@ local securitykey = math.random(111111,999999)
 local resource = GetCurrentResourceName()
 local codeloaded = false
 
-RegisterNetEvent(resource.."unknown_antidump:client:load", function(code,key)
+RegisterNetEvent(resource.."unknown_antidump:client:load", function(files,key)
     if key ~= securitykey or codeloaded then
         print("Netter Versuch!")
         return
     end
-    assert(load(code, "Error:"))() --Load and execute Code
+
+    --Load Code--
+    for k, v in ipairs(files) do
+        local success, err = load(v.code, v.file, "bt") 
+        if not success then
+            -- Fehler Handling
+            local cleanError = string.match(err, ":%d+:%s*(.*)") or err
+            local lineNumber = string.match(err, ":(%d+):") or "?"
+            print("^1Error^0 [Zeile - " ..lineNumber.. "] " ..cleanError)
+        else
+            --Execute Code--
+            pcall(success)
+            -----------------
+            print("Datei: ^3" .. v.file .. "^0 wurde ^2erfolgreich^0 geladen!") 
+        end
+    end
+    ------------
+
+
     codeloaded = true
 end)
 
