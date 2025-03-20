93d359b02e64d4f7d672c47b0cf0eec698653930
Unknown_user410 <christianhenrich16@gmail.com>
2025-03-20 19:57:35 +0100

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,5 +2,7 @@ fx_version 'cerulean'
 games {'gta5'}
 author "Unknown_user410"
 description "Anti Dumper"
-version "1.1.2"
+version "2.0.0"
+
+client_script 'client/example.lua'
 server_script 'server/server.lua'
